873b90c471bc6dcb927dc28e42694979b4d42270
basic-app <dev@basic-app.com>
2024-02-14 23:42:36 +0300

    n/a

--- Config/Events.php
+++ Config/Events.php
@@ -3,10 +3,11 @@
 use BasicApp\Helpers\Url;
 use BasicApp\Admin\AdminEvents;
 use BasicApp\Message\Controllers\Admin\Message as MessageController;
+use BasicApp\AdminMenu\AdminMenuEvents;
 
-if (class_exists(AdminEvents::class))
+if (class_exists(AdminMenuEvents::class))
 {
-    AdminEvents::onMainMenu(function($menu) 
+    AdminMenuEvents::onMainMenu(function($menu) 
     {
         $menu->items['system']['items']['message'] = [
             'url'   => Url::createUrl('admin/message'),
